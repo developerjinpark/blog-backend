cb34361ce25aa63952fc2ebc0c710d6815821d50
Jin <jinpark1504@gmail.com>
2020-04-02 15:42:19 -0400

    added posts router

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,7 @@
 const Router = require('koa-router');
-
+const posts = require('./posts');
 const api = new Router();
 
-api.get('/test', (ctx) => {
-    ctx.body = 'Test was succeed';
-});
+api.use('/posts', posts.routes());
 
 module.exports = api;
