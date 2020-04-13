7fa11fc20b5af2cc456dbbd2e3aa21b65c6de7f8
Jin <jinpark1504@gmail.com>
2020-04-13 13:32:22 -0400

    get list by searching a tag

--- src/api/posts/posts.ctrl.js
+++ src/api/posts/posts.ctrl.js
@@ -51,6 +51,9 @@ exports.write = async (ctx) => {
 
 exports.list = async (ctx) => {
     const page = parseInt(ctx.query.page || 1, 10);
+    const { tag } = ctx.query;
+
+    const query = tag ? { tags: tag } : {};
 
     if (page < 1) {
         ctx.status = 400;
@@ -58,25 +61,26 @@ exports.list = async (ctx) => {
     }
 
     try {
-        const posts = await Post.find()
+        const posts = await Post.find(query)
             .sort({_id:-1})
             .limit(10)
             .skip((page - 1) * 10)
             .lean()
             .exec();
-        const postCount = await Post.count().exec();
+
+        const postCount = await Post.count(query).exec();
 
         const limitBodyLength = post => ({
             // ...post.toJSON(),
             ...post,
-            body: post.body.length < 200 ? post.body : `${post.body.slice(0,200)}...`
+            body: post.body.length < 350 ? post.body : `${post.body.slice(0,350)}...`
         });
         ctx.body = posts.map(limitBodyLength);
 
         ctx.set('Last-Page', Math.ceil(postCount / 10));
         // ctx.body = posts;
     } catch(e) {
-        ctx.throw(e, 500);
+        ctx.throw(500, e);
     }
 };
 
